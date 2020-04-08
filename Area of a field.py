c197db07d1029ce2414b23845ca568a4eda95b06
Jordan So <jordan.so21@ycdsbk12.ca>
2020-04-08 18:23:21 -0400

    Area of a field

--- Area of a field.py
+++ Area of a field.py
@@ -4,8 +4,8 @@ width = input("Enter a width in meters: ")
 length = int(length)
 width = int(width)
 
-area = (length * width)
-area = str(area)
-print("The area of the field is: " + area + " meters")
-
+area = length * width
+acres = area // 43560
+acres = str(acres)
+print("The area of the field is " + acres + " acres")
 
