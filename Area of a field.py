01d19f81a4cb6a0cee53819bda4c21e2c8c19379
Jordan-1234 <58566042+Jordan-1234@users.noreply.github.com>
2020-04-08 13:51:34 -0400

    Add files via upload

---
+++ Area of a field.py
@@ -0,0 +1,11 @@
+length = input("Enter a length in meters: ")
+width = input("Enter a width in meters: ")
+
+length = int(length)
+width = int(width)
+
+area = (length * width)
+area = str(area)
+print("The area of the field is: " + area + " meters")
+
+
