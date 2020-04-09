e701c224334c04dea4a20ac8b97e240cfa9fce99
Jordan So <jordan.so21@ycdsbk12.ca>
2020-04-08 21:00:04 -0400

    volume of a cylinder

--- Volume of a Cylinder.py
+++ Volume of a Cylinder.py
@@ -1,3 +1,6 @@
 import math
-radius = float(input("Enter the radius of the cylinder"))
-height = float(input("Enter the height of the cylinder"))
+radius = float(input("Enter in the radius of the cylinder "))
+height = float(input("Enter in the height of the cylinder "))
+cylinder_Area = (2 * math.pi * pow(radius,2) * height)
+cylinder_Area = str(cylinder_Area) 
+print = ('The volume of the cylinder will be ' + cylinder_Area) 
