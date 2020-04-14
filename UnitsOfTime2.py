b72860ccf10267bc725426f6c156f5036d1f619a
Jordan So <jordan.so21@ycdsbk12.ca>
2020-04-14 01:10:33 -0400

    UnitsOfTime2

--- UnitsOfTime2.py
+++ UnitsOfTime2.py
@@ -6,17 +6,15 @@ seconds_In_A_Day = 86400
 #User input for the number of seconds
 seconds = int(input("What is the total amount of seconds: "))
 
-# Find out the days, hours, minutes, seconds
-days = seconds / seconds_In_A_Day
+#Find out the days, hours, minutes, seconds
+days = seconds // seconds_In_A_Day
 seconds = seconds % seconds_In_A_Day
 
-hours = seconds / seconds_In_A_Hour
-seconds = seconds % seconds_In_A_Day
+hours = seconds // seconds_In_A_Hour
+seconds = seconds % seconds_In_A_Hour
 
-minutes = seconds / seconds_In_A_Minute
+minutes = seconds // seconds_In_A_Minute
 seconds = seconds % seconds_In_A_Minute
 
-print("The number of days is: " + str(round(days,1))) 
-print("The number of hours is: " + str(round(hours,1)))
-print("The number of minutes is: " + str(round(minutes,1)))
-print("The number of seconds is: " + str(seconds))
+#Conclusion using f strings 
+print(f"The duration is: {days}:{hours}:{minutes}:{seconds}")
