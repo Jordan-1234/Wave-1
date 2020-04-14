fd8d8051ccebb56565d907ef29e96af874513c8f
Jordan So <jordan.so21@ycdsbk12.ca>
2020-04-14 00:17:47 -0400

    UnitsOfTime

---
+++ UnitsOfTime.py
@@ -0,0 +1,5 @@
+# Convert each Variable to seconds
+Days = int(input("What is the number of days")) 
+Hours = int(input("What is the number of hours"))
+Minutes = int(input("what is the number of minutes"))
+Seconds = int(input("What is the number of seconds"))
